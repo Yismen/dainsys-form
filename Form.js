0548a3d53ec082c66a686ec81f673511354a7130
Yismen Jorge <yismen.jorge@gmail.com>
2017-05-08 14:18:51 -0400

    first commit

---
+++ Form.js
@@ -0,0 +1,75 @@
+let Error = require('./Error.js').default;
+let Http = Vue.http;
+export default class Form {
+    constructor (data) {
+        this.originalData = data;
+        this.fields = {}
+
+        for (let field in data) {
+            this.fields[field] = data[field];
+        }
+
+        this.error = new Error();
+
+        this.http = Http;
+    }
+
+    submit(requestType, url) {
+        requestType = requestType.toLowerCase();
+        let vm = this;
+        return this.http[requestType](url, vm.fields)
+            // .then(this.onSuccess.bind(this))
+            .catch(this.onFail.bind(this));
+    }
+
+    onSuccess(response) {
+        this.error.clear();
+        return response;
+    }
+
+    onFail(errors) {
+        this.error.record(errors.data);
+    }
+
+    get(url) {
+        return this.submit('get', url);
+    }
+
+    post(url) {
+        return this.submit('post', url);
+    }
+
+    put(url) {
+        return this.submit('put', url);
+    }
+
+    delete(url) {
+        return this.submit('delete', url);
+    }
+
+    resetField(field) {
+        this.fields[field] = '';
+    }
+
+    reset(field) {
+        if (field) { 
+            return this.fields[field] = '';
+        }
+
+        for(let field in this.fields) {
+            this.fields[field] = '';
+        }
+    }
+
+    // reset(fieldsArray) {
+    //     let fields = this.fields;
+    //     if (fieldsArray) {
+    //         fields = fieldsArray;
+    //     } 
+
+    //     for(let field in fields) {
+    //         console.log(field)
+    //         this.fields[field] = '';
+    //     }
+    // }
+}
